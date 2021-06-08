3e4ff4eff05eaf1ad484a97d1dab867d1ec17209
Leandro Moreira <leandrodev.oliveira@gmail.com>
2021-06-07 21:43:17 -0300

    Projeto criado

---
+++ backend/src/test/java/com/desafiods/crudcliente/CrudClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiods.crudcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
